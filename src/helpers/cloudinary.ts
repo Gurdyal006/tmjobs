3e1dffddef2a578de41c2eb5604beca8793949f7
Gurdyal Singh <102032512+Gurdyal006@users.noreply.github.com>
2024-04-16 19:57:15 +0530

    Update cloudinary.ts

--- src/helpers/cloudinary.ts
+++ src/helpers/cloudinary.ts
@@ -1,17 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-// configure cloudinary
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+configure cloudinary
 cloudinary.config({
-  cloud_name: "dmsyoa2lq", //process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: "649278495318135", //process.env.CLOUDINARY_API_KEY,
-  api_secret: "YnVr4AUupx6Ep-60c106FTx_2dk", // process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 const uploadOnCloudinary = async (localFilePath: any) => {
   try {
